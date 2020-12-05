fe1bdff6ae5da3c43b73c07ee84d5982d77bb65c
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2020-12-05 20:04:25 -0300

    criando class Artista

---
+++ p2-java-eventos/lib/src/main/java/com/projetop2/model/Artista.java
@@ -0,0 +1,22 @@
+package com.projetop2.model;
+
+
+public class Artista {
+
+	private String nomeArtista;
+	private String estiloMusical;
+	public String getNomeArtista() {
+		return nomeArtista;
+	}
+	public void setNomeArtista(String nomeArtista) {
+		this.nomeArtista = nomeArtista;
+	}
+	public String getEstiloMusical() {
+		return estiloMusical;
+	}
+	public void setEstiloMusical(String estiloMusical) {
+		this.estiloMusical = estiloMusical;
+	}
+	
+	
+}
