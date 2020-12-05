8bddc1179b78e04be30467a22eb50f42273800ab
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2020-12-05 19:57:27 -0300

    adicionando o application.yml

---
+++ p2-java-eventos/lib/bin/main/schema-postgresql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS evento (
+	
+	id serial primary key,
+	nm_evento varchar(50),
+	local varchar(50),	 
+);
+
+CREATE TABLE IF NOT EXISTS convidado (
+	id serial primary key,
+	nm_convidado varchar(50),
+	rg varchar(50)	
+);
+
+CREATE TABLE IF NOT EXISTS artista (
+
+	id serial primary key,
+	nm_artista varchar(50),
+	nm_estilo_musical varchar(50)
+);
