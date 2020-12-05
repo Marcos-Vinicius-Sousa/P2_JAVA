9bb11ec7f50a4bcfc428ae35c1a95a970c8447b1
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2020-12-05 20:44:44 -0300

    convidadoService

--- p2-java-eventos/lib/src/main/java/com/projetop2/model/ConvidadoService.java
+++ p2-java-eventos/lib/src/main/java/com/projetop2/model/ConvidadoService.java
@@ -1,5 +1,35 @@
 package com.projetop2.model;
 
+import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ConvidadoService {
 
+	
+	@Autowired 
+    ConvidadoDAO cdao;
+
+    public void insert(Convidado con) {
+      cdao.insert(con);
+    }
+    
+    public Map<String, Object> getConvidado(int id) {
+		  return cdao.getConvidado(id);
+	  }
+	  
+    public List<Map<String, Object>> getConvidados() {
+	      return cdao.getConvidados();
+	  }
+	  
+	  public void deleteConvidado(int id) {
+		  cdao.deleteConvidado(id);
+	  }
+	  
+	  public void updateConvidado(int id, Convidado cnd) {
+		  cdao.updateConvidado(id, cnd);
+	  }
 }
