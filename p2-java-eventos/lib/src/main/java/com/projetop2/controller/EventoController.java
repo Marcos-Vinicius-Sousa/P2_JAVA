f8524a5139b6c8e32b2cda5518a606d0db2dd1b3
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2020-12-06 18:18:11 -0300

    eventoupd

--- p2-java-eventos/lib/src/main/java/com/projetop2/controller/EventoController.java
+++ p2-java-eventos/lib/src/main/java/com/projetop2/controller/EventoController.java
@@ -1,6 +1,7 @@
 package com.projetop2.controller;
 
 import java.util.List;
+
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
--- p2-java-eventos/lib/src/main/java/com/projetop2/controller/EventoUpdController.java
+++ p2-java-eventos/lib/src/main/java/com/projetop2/controller/EventoUpdController.java
@@ -1,5 +1,38 @@
 package com.projetop2.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.projetop2.model.Evento;
+import com.projetop2.model.EventoService;
+
+@Controller
 public class EventoUpdController {
 
+	@Autowired
+	private ApplicationContext context;
+	
+	@GetMapping("/upd/{id}")
+    public String updateFormEvt(@PathVariable("id") int id, Model model){
+		EventoService edao = context.getBean(EventoService.class);
+		Map<String,Object> antigo = edao.getEvento(id);
+		Evento evt = new Evento((String)antigo.get("nome"),(String)antigo.get("local"));
+		model.addAttribute("antigo",evt);
+		model.addAttribute("id",id);
+		return "formevent";
+    }
+	
+	@PostMapping("/upd/{id}")
+	public String updateEv(@PathVariable("id") int id,@ModelAttribute Evento evt, Model model) {
+		EventoService edao = context.getBean(EventoService.class);
+		edao.updateEvento(id, evt);
+		return "redirect:/eventos";
+	}
 }
